94a861431312287424fee1119224b233a1694c38
Conor Macken <conormacken@Conors-MacBook-Pro-2.local>
2013-12-05 18:46:04 -0800

    demand login rspec done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,37 @@
 require 'spec_helper'
+# In order to gain access to the site,
+# as demand user
+# I want to be able to login.
 
-describe User do
-  before do 
-  	@userfail = User.create(username: nil, name: nil)
-  end
+# Scenario 1: Success
+# When I add a species
+# and I add a color
+# and I click 'save'
+# Then the page should display the
+# color and species
+# and the animal is saved
 
-  context "creating a user"
-	  
-	  it "will validate the presense of a username" do
-	 			expect(@userfail.username).to_not be_valid
-	  end
 
-	  it "will validate the presense of a name" do
-	 			expect(@userfail.name).to_not be_valid
-	  end
+feature "demand user creates an account" do
+	context "susessfully" do
+		scenario "creating an account" do
+			visit '/'
+
+			fill_in 'username', with:'tepeh'
+			fill_in 'name', with: 'tim'
+			fill_in 'email', with: 'tim@gmail.com'
+			fill_in 'type', with: 'demand'
+
+			click_button 'submit'#it might be save
+
+			expect(current_path).to eql users_path
+
+			expect(page).to have_content 'tepeh'
+			expect(page).to have_content 'Welcome'
+		
+
+		end 	
+	end  
 end
+
+
