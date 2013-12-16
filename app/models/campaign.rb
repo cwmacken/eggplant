dad4f9ea23860708d39728713d849643f84345b5
Conor Macken <conormacken@Conors-MacBook-Pro-2.local>
2013-12-16 14:24:14 -0800

    delete button gone and validating presense of number in price field

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,9 @@
 class Campaign < ActiveRecord::Base
+	validates_presence_of :price 
+	validates_presence_of :description
+	validates_presence_of :title
+	validates_numericality_of :price, :on => :create
+
 	belongs_to :user
 	has_many :submissions
 end
